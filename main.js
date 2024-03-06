76cf57a65f28685e15acb75cae9b2ff37facd7a9
coploc017 <122494228+coploc017@users.noreply.github.com>
2024-03-06 16:09:58 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+noseX=0;
+noseY=0;
+
+
+function preload() {
+lentes =loadImage('https://i.postimg.cc/BvK3J6r9/imageedit-4-2844835077.png')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet esta inicializado');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(lentes, noseX -50, noseY -50, 90, 60);
+
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
